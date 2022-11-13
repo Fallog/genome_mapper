963c3e4725e9bcbb194ddde46619776fda5bb80b
Fallog <fallog@mailo.com>
2022-11-13 14:21:06 +0100

    Advancing DC3.py Function added : getIndexTable ; getOrderTable Modified function -> createNextIntList

--- dc3_fallog.py
+++ dc3_fallog.py
@@ -1,5 +1,5 @@
-import cProfile
-import random
+# import cProfile
+# import random
 from radix_sort_tuple_fallog import radixSort
 
 
@@ -51,21 +51,21 @@ def getPositionTable(input_list: list[int]) -> list[int]:
     return P1 + P2
 
 
-def getOrderedTripletList(
+def getTripletListIndexed(
     input_list: list[int], position_table: list[int]
 ) -> list[tuple[tuple[int, int, int], int]]:
-    """Returns a list of tuple (triplet, 1).
+    """Returns a list of tuple made of a triplet plus its index in the position table.
     The triplets are built using the position table and an array of integers.
-    For each element p in the position table, we form a triplet of integers by extracting :
-    (input_list[p], input_list[p + 1], input_list[p + 2])
+    For each element p in the position table, we form a tuple looking like this :
+    ((input_list[p], input_list[p + 1], input_list[p + 2]), p)
 
     Args:
         input_list (list[int]): an array of integers
         position_table (list[int]): array obtained by the getPositionTable function
 
     Returns:
-        list[tuple[tuple[int, int, int], int]]: list of tuple, each tuple consisting in a triplet and
-        an integer, set to 1 in this function
+        list[tuple[tuple[int, int, int], int]]: list of tuple where a tuple consists in a triplet and
+    an integer representing the index from which the triplet is built
     """
     pos_size = len(position_table)
     triplet_list = [0] * pos_size
@@ -77,49 +77,89 @@ def getOrderedTripletList(
                 input_list[position + 1],
                 input_list[position + 2],
             ),
-            1,
+            position,
         )
     return triplet_list
 
 
 def getTripletList(
-    ordered_triplet_list: list[tuple[tuple, int]]
+    index_triplet_list: list[tuple[tuple, int]]
 ) -> list[tuple[int, int, int]]:
     """Returns a list of triplets, extracted from a list of tuple where a tuple consists in a triplet and
     an integer representing the order of the triplet
 
     Args:
-        ordered_triplet_list (list[tuple[tuple, int]]): list of tuple where a tuple consists in a triplet and
-    an integer representing the order of the triplet
+        index_triplet_list (list[tuple[tuple, int]]): list of tuple where a tuple consists in a triplet and
+    an integer representing the index from which the triplet was built in the first place
 
     Returns:
-        list[tuple[int, int, int]]: list made of the triplets of ordered_triplet_list without the order
+        list[tuple[int, int, int]]: list made of the triplets of ordered_triplet_list without the index
     """
-    return [ordered_triplet_list[i][0] for i in range(len(ordered_triplet_list))]
+    return [index_triplet_list[i][0] for i in range(len(index_triplet_list))]
 
 
-def createNewIntList(
-    triplet_list: list[tuple[tuple[int, int, int], int]],
-    ordered_triplet_list: list[tuple[tuple, int]],
-) -> list[int]:
-    """Returns an array of the orders of the triplets in triplet_list.
+def getIndexTable(triplet_list: list[tuple[tuple[int, int, int], int]]) -> list[int]:
+    """Returns an array of integers built with the int inside the tuple of triplet_list.
+    triplet_list is an array of ((int, int, int), int) and here it extracts the last int and put it in the returned list.
 
     Args:
-        triplet_list (list[tuple[tuple[int, int, int], int]]): list of tuple where a tuple consists in a
-        triplet and an integer representing the order of the triplet
-        ordered_triplet_list (list[tuple[tuple[int, int, int], int]]): copy of triplet_list sorted in ascending order
+        triplet_list (list[tuple[tuple[int, int, int], int]]): a tuple consists in a triplet and an integer representing
+        the index from which the triplet was built in the first place
 
     Returns:
-        list[int]: a new sequence of integers built with the orders given by ordered_triplet_list but using the orders of
-        the triplets in triplet_list.
+        list[int]: indexes of the triplet in an sorted or unsorted triplet_list
     """
-    size = len(triplet_list)
-    triplet_list_unordered = getTripletList(ordered_triplet_list)
-    new_int_list = [0] * size
+    return [triplet_list[i][1] for i in range(len(triplet_list))]
+
+
+def getOrderTable(
+    sorted_tuple_list: list[tuple[tuple[int, int, int], int]]
+) -> list[int]:
+    """Returns the sorted position of each triplet in sorted_tuple_list.
+    If two triplet are identical, their position is set equal.
+
+    Args:
+        sorted_tuple_list (list[tuple[tuple[int, int, int], int]]): a tuple consists in a triplet and an integer
+        representing the index from which the triplet was built in the first place
+
+    Returns:
+        list[int]: sorted positions of the triplets
+    """
+    size = len(tuple_list)
+    order_table = [1] * size
+
+    for i in range(1, size):
+        parsed_tuple = sorted_tuple_list[i][0]
+        previous_tuple = sorted_tuple_list[i - 1][0]
+        if parsed_tuple == previous_tuple:
+            order_table[i] = order_table[i - 1]
+        else:
+            order_table[i] = order_table[i - 1] + 1
+    return order_table
+
+
+def createNextIntList(
+    index_table: list[int], index_table_sort: list[int], order_table: list[int]
+) -> list[int]:
+    """Returns an array made of the index + 1 in index_table_sort of each element of index_table.
+
+    Args:
+        index_table (list[int]): indexes of the triplet in an unsorted triplet_list
+        index_table_sort (list[int]): indexes of the triplet in a sorted triplet_list
+        order_table (list[int]): orders of the triplet in a sorted triplet_list
+
+    Returns:
+        list[int]:
+    """
+    size = len(index_table)
+    next_int_list = [0] * size
     for i in range(size):
-        new_int = triplet_list_unordered.index(triplet_list[i][0])
-        new_int_list[i] = ordered_triplet_list[new_int][1]
-    return new_int_list
+        next_int_list[i] = order_table[index_table_sort.index(index_table[i])]
+    return next_int_list
+
+
+def DC3(int_sequence: list[int]):
+    pass
 
 
 if __name__ == "__main__":
@@ -134,21 +174,22 @@ if __name__ == "__main__":
     pos_table = getPositionTable(seq_int)
     print(pos_table == [1, 4, 7, 10, 2, 5, 8])  # OK
 
-    triplet_list = getOrderedTripletList(seq_int, pos_table)
+    tuple_list = getTripletListIndexed(seq_int, pos_table)
     print(
-        triplet_list
+        tuple_list
         == [
             ((4, 4, 1), 1),
-            ((3, 2, 1), 1),
-            ((3, 2, 2), 1),
-            ((0, 0, 0), 1),
-            ((4, 1, 3), 1),
-            ((2, 1, 3), 1),
-            ((2, 2, 0), 1),
+            ((3, 2, 1), 4),
+            ((3, 2, 2), 7),
+            ((0, 0, 0), 10),
+            ((4, 1, 3), 2),
+            ((2, 1, 3), 5),
+            ((2, 2, 0), 8),
         ]
     )  # OK
+
     print(
-        getTripletList(triplet_list)
+        getTripletList(tuple_list)
         == [
             (4, 4, 1),
             (3, 2, 1),
@@ -159,8 +200,31 @@ if __name__ == "__main__":
             (2, 2, 0),
         ]
     )  # OK
-    sorted_triplet_list = radixSort(triplet_list)
-    print(sorted_triplet_list)
+
+    sorted_triplet_list = radixSort(tuple_list)
     print(
-        createNewIntList(triplet_list, sorted_triplet_list) == [6, 4, 5, 1, 7, 2, 3]
+        sorted_triplet_list
+        == [
+            ((0, 0, 0), 10),
+            ((2, 1, 3), 5),
+            ((2, 2, 0), 8),
+            ((3, 2, 1), 4),
+            ((3, 2, 2), 7),
+            ((4, 4, 1), 1),
+            ((4, 1, 3), 2),
+        ]
+    )
+
+    index_list = getIndexTable(tuple_list)
+    print(index_list == [1, 4, 7, 10, 2, 5, 8])  # OK
+
+    index_list_sorted = getIndexTable(sorted_triplet_list)
+    print(index_list_sorted == [10, 5, 8, 4, 7, 1, 2])  # OK
+
+    order_table = getOrderTable(sorted_triplet_list)
+    print(order_table == [1, 2, 3, 4, 5, 6, 7])  # OK
+
+    print(
+        createNextIntList(index_list, index_list_sorted, order_table)
+        == [6, 4, 5, 1, 7, 2, 3]
     )  # OK
