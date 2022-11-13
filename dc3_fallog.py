bf6a6643d1df4f8f2ba85957151370cac6133479
Fallog <fallog@mailo.com>
2022-11-13 11:36:35 +0100

    Updating documentations

--- dc3_fallog.py
+++ dc3_fallog.py
@@ -36,7 +36,7 @@ def appendSentinelNumbers(input_list: list[int]) -> list:
 
 def getPositionTable(input_list: list[int]) -> list[int]:
     """Returns the concatenation of an array containing indexes i of input_list that satisfy i % 3 = 1 and
-    another array containing indexes satisfying i % 3 = 2. Also i = 2 < len(input_list)
+    another array containing indexes satisfying i % 3 = 2. Also i + 2 < len(input_list).
 
     Args:
         input_list (list[int]): an array of integers
@@ -53,7 +53,20 @@ def getPositionTable(input_list: list[int]) -> list[int]:
 
 def getOrderedTripletList(
     input_list: list[int], position_table: list[int]
-) -> list[tuple[tuple, int]]:
+) -> list[tuple[tuple[int, int, int], int]]:
+    """Returns a list of tuple (triplet, 1).
+    The triplets are built using the position table and an array of integers.
+    For each element p in the position table, we form a triplet of integers by extracting :
+    (input_list[p], input_list[p + 1], input_list[p + 2])
+
+    Args:
+        input_list (list[int]): an array of integers
+        position_table (list[int]): array obtained by the getPositionTable function
+
+    Returns:
+        list[tuple[tuple[int, int, int], int]]: list of tuple, each tuple consisting in a triplet and
+        an integer, set to 1 in this function
+    """
     pos_size = len(position_table)
     triplet_list = [0] * pos_size
     for i in range(pos_size):
@@ -69,23 +82,44 @@ def getOrderedTripletList(
     return triplet_list
 
 
-def getTripletList(ordered_triplet_list: list[tuple[tuple, int]]) -> list[tuple]:
-    triplet_list = [0] * len(ordered_triplet_list)
-    for index, order_tuple in enumerate(ordered_triplet_list):
-        triplet_list[index] = order_tuple[0]
-    return triplet_list
+def getTripletList(
+    ordered_triplet_list: list[tuple[tuple, int]]
+) -> list[tuple[int, int, int]]:
+    """Returns a list of triplets, extracted from a list of tuple where a tuple consists in a triplet and
+    an integer representing the order of the triplet
+
+    Args:
+        ordered_triplet_list (list[tuple[tuple, int]]): list of tuple where a tuple consists in a triplet and
+    an integer representing the order of the triplet
+
+    Returns:
+        list[tuple[int, int, int]]: list made of the triplets of ordered_triplet_list without the order
+    """
+    return [ordered_triplet_list[i][0] for i in range(len(ordered_triplet_list))]
 
 
-def createNewIntTable(
-    triplet_list: list[tuple[tuple, int]], triplet_list_ordered: list[tuple[tuple, int]]
+def createNewIntList(
+    triplet_list: list[tuple[tuple[int, int, int], int]],
+    ordered_triplet_list: list[tuple[tuple, int]],
 ) -> list[int]:
+    """Returns an array of the orders of the triplets in triplet_list.
+
+    Args:
+        triplet_list (list[tuple[tuple[int, int, int], int]]): list of tuple where a tuple consists in a
+        triplet and an integer representing the order of the triplet
+        ordered_triplet_list (list[tuple[tuple[int, int, int], int]]): copy of triplet_list sorted in ascending order
+
+    Returns:
+        list[int]: a new sequence of integers built with the orders given by ordered_triplet_list but using the orders of
+        the triplets in triplet_list.
+    """
     size = len(triplet_list)
-    triplet_list_unordered = getTripletList(triplet_list_ordered)
-    new_int_table = [0] * size
+    triplet_list_unordered = getTripletList(ordered_triplet_list)
+    new_int_list = [0] * size
     for i in range(size):
         new_int = triplet_list_unordered.index(triplet_list[i][0])
-        new_int_table[i] = triplet_list_ordered[new_int][1]
-    return new_int_table
+        new_int_list[i] = ordered_triplet_list[new_int][1]
+    return new_int_list
 
 
 if __name__ == "__main__":
@@ -128,5 +162,5 @@ if __name__ == "__main__":
     sorted_triplet_list = radixSort(triplet_list)
     print(sorted_triplet_list)
     print(
-        createNewIntTable(triplet_list, sorted_triplet_list) == [6, 4, 5, 1, 7, 2, 3]
+        createNewIntList(triplet_list, sorted_triplet_list) == [6, 4, 5, 1, 7, 2, 3]
     )  # OK
