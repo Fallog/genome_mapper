8f3339f0fb87b7c120f6857f8650f1db71dd3e08
f2ire <yanisfiire@gmail.com>
2022-11-25 13:49:09 +0100

    [REFACTORING] Refactoring of some function
    - DC3 based fun are now in dc3 files. Others in tools
    - quickSort --> computeAllVariable. More adapted to this project

--- tools.py
+++ tools.py
@@ -3,45 +3,75 @@ import random
 import numpy as np
 
 
-def quickSort(x):
-    """
-    Return index list and T\'. The x array in parameter is sorted.
-    """
-    x_copy = np.copy(x)
+def strToAscii(text: str) :  # ya un numpy loadtxt qui existe
+    """Transform a string to is Ascii value ant put it in a numpy array
+    TO DO : update for DNA (A --> 1 : C --> 2 : G --> 3 : T --> 4)
 
-    index_list = np.argsort(x, kind="mergesort", order=("1", "2", "3"))
-    x.sort(kind="quicksort", order=("1", "2", "3"))
-    tp = np.empty(x.size)
-    array_without_duplicate = np.unique(x)
-    for i, elem in enumerate(x_copy):
-        tp[i] = np.where(elem == array_without_duplicate)[0][0] + 1
+    Args:
+        text (str): DNA sequence
 
-    return index_list, tp
+    Returns:
+        array: Number array (representing the DNA sequence)
+    """
+    res_array = np.frombuffer(text.encode(), np.int8)
+    return res_array
+
+
+def strToBase(text: str):
+    text = text.upper()
+    res_array = np.empty(len(text))
+    for i in range(len(text)):
+        if text[i] == "A":
+            res_array[i] = 1
+        elif text[i] == "C":
+            res_array[i] = 2
+        elif text[i] == "G":
+            res_array[i] = 3
+        else:
+            res_array[i] = 4
+    return res_array
+
+
+def asciiToStr(ascii_list) -> str:
+    """Transform a Numpy array of int to a string
+    TO DO : adapt to ACGT
+
+    Args:
+        ascii_list (array): Numpay array of int
+
+    Returns:
+        str: String representative of the initial array
+    """
+    res_txt = ascii_list.tobytes().decode("ascii")
+    return res_txt
 
 
 if __name__ == "__main__":
 
-    liste = np.array(
-        [
-            (5, 28, 1),
-            (9, 111, 9),
-            (9, 1, 5),
-            (99, 1, 9),
-            (2, 1, 89),
-            (29, 19, 1),
-        ],
-        dtype=[("1", int), ("2", int), ("3", int)],
-    )
+    # liste = np.array(
+    #     [
+    #         (5, 28, 1),
+    #         (9, 111, 9),
+    #         (9, 1, 5),
+    #         (99, 1, 9),
+    #         (2, 1, 89),
+    #         (29, 19, 1),
+    #     ],
+    #     dtype=[("1", int), ("2", int), ("3", int)],
+    # )
 
-    random.seed(12)
-    # datatest = [(randint(1, 4), randint(1, 4), randint(1, 4)) for i in range(5000)]
+    # random.seed(12)
+    # datatest = [
+    #     (random.randint(1, 4), random.randint(1, 4), random.randint(1, 4))
+    #     for i in range(500000)
+    # ]
 
     # datatest = np.array(datatest, dtype=[("1", int), ("2", int), ("3", int)])
 
-    # print(datatest)
+    # # print(datatest)
 
-    print(liste)
-    res = quickSort(liste)
-    print(liste)
-    print(res)
-    cProfile.run("quickSort(liste)")
+    # print(liste)
+    # res = quickSort(liste)
+    # print(liste)
+    # print(res)
+    # cProfile.run("quickSort(datatest)")
--- y_dc3.py
+++ y_dc3.py
@@ -4,49 +4,6 @@ import numpy as np
 import tools
 
 
-def strToAscii(text: str) -> array:  # ya un numpy loadtxt qui existe
-    """Transform a string to is Ascii value ant put it in a numpy array
-    TO DO : update for DNA (A --> 1 : C --> 2 : G --> 3 : T --> 4)
-
-    Args:
-        text (str): DNA sequence
-
-    Returns:
-        array: Number array (representing the DNA sequence)
-    """
-    res_array = np.frombuffer(text.encode(), np.int8)
-    return res_array
-
-
-def strToBase(text: str) -> array:
-    text = text.upper()
-    res_array = np.empty(len(text))
-    for i in range(len(text)):
-        if text[i] == "A":
-            res_array[i] = 1
-        elif text[i] == "C":
-            res_array[i] = 2
-        elif text[i] == "G":
-            res_array[i] = 3
-        else:
-            res_array[i] = 4
-    return res_array
-
-
-def asciiToStr(ascii_list: array) -> str:
-    """Transform a Numpy array of int to a string
-    TO DO : adapt to ACGT
-
-    Args:
-        ascii_list (array): Numpay array of int
-
-    Returns:
-        str: String representative of the initial array
-    """
-    res_txt = ascii_list.tobytes().decode("ascii")
-    return res_txt
-
-
 def makeP12(t_list: array) -> array:
     """Create P12 : Take all the n+1%3 and n+2%3 iif < N-2
 
@@ -79,57 +36,9 @@ def makeTriplet(t_list: array, p12_list: array) -> array:
     return r12
 
 
-def sortTriplet(r12: list[list[int]]) -> list[list[int]]:
-    return tools.quickSort(r12)
-
-
-def makeDict(
-    p12: array, r12: array, r12s: array, index_list: array, tp: array
-) -> dict:  # !! USELESS !!
-    """Make a dict to keep all data needed for this algorithm
-    TO DO : Deep only data needed
-
-    Args:
-        p12 (array): p12 array (Index of T kept with n+1 and n+2 % 3)
-        r12 (array): Triplet array no sorted
-        r12s (array): Triplet array sorted
-        index_list (array): Array of index of each triplet before the sort
-        tp (array): Array of index for each triplet after the sort, when it was before
-
-    Returns:
-        dict: Dict contening all data
-    """
-    index = np.take_along_axis(np.copy(p12), index_list, axis=0)
-    i_dict = {
-        "p12": p12,
-        "r12": r12,
-        "r12s": r12s,
-        "index": index,
-        "tp": tp,
-    }
-    return i_dict
-
-
-def print_dict(iter_dict: dict) -> None:  # !! USELESS !!
-    for key, vals in iter_dict.items():
-        print(f"The {key} is : {vals}")
-
-
-def print_iters_dict(iters_dict: dict) -> None:  # !! USELESS !!
-    for key, vals in iters_dict.items():
-        print(f"For the iteration number --- {key} --- :\n")
-        print_dict(vals)
-
-
 def reccursive_sort_s11(init_seq, iter=0):
     T = np.concatenate((init_seq, np.zeros(3)))
-    p12 = makeP12(T)
-    r12 = makeTriplet(T, p12)
-    r12s = np.copy(r12)
-    index_list, tp = sortTriplet(r12s)
-    index = np.take_along_axis(np.copy(p12), index_list, axis=0)
-    # d = makeDict(p12, r12, r12s, index_list, tp)  !! USELESS !!
-    # iter_dict[str(iter)] = d                      !! USELESS !!
+    p12, r12, r12s, index, tp = computeDc3Variable(T)
 
     if tp.shape == np.unique(tp).shape:  # step 2 ?
         print("on est la")
@@ -139,12 +48,42 @@ def reccursive_sort_s11(init_seq, iter=0):
     print(tp, iter)  # STILL WORKING !
 
 
+def computeDc3Variable(T):
+    """
+    Compute all DC3 variable computed in the first step and return it
+
+    Args:
+        T (array) : Array of all the values
+
+    Returns:
+        p12 (array): p12 array (Index of T kept with n+1 and n+2 % 3)
+        r12 (array): Triplet array no sorted
+        r12s (array): Triplet array sorted
+        index (array): TODO: FIND WAY TO EXPLAIN THIS
+        tp (array): Array of index for each triplet after the sort, when it was before (T\')
+    """
+
+    p12 = makeP12(T)
+    r12 = makeTriplet(T, p12)
+    r12s = np.copy(r12)
+    index_list = np.argsort(r12s, kind="mergesort", order=("1", "2", "3"))
+    index = np.take_along_axis(np.copy(p12), index_list, axis=0)
+
+    r12s.sort(kind="quicksort", order=("1", "2", "3"))  # SORT HERE
+    tp = np.empty(r12s.size)
+    array_without_duplicate = np.unique(r12s)
+    for i, elem in enumerate(r12):
+        tp[i] = np.where(elem == array_without_duplicate)[0][0] + 1
+
+    return p12, r12, r12s, index, tp
+
+
 def step2():
     pass
 
 
-def dc3(seq: str):
-    T = strToAscii(seq)
+def dc3(seq: str):  # TODO: Changer recursive_sort_s11 en DC3
+    T = tools.strToAscii(seq)
     reccursive_sort_s11(T)
     # print(iter_dict)
     # print_iters_dict(iter_dict)
