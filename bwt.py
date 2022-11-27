302b4ecdbede03f44b75fbee76b284bbfbbe5e37
Fallog <fallog@mailo.com>
2022-11-27 15:36:48 +0100

    Changing testing sequence

--- bwt.py
+++ bwt.py
@@ -132,33 +132,17 @@ def pattern_matching_BWT(S, pattern):
 
             i -= 1
 
-    # Wrong if the first letter is not in pattern argument
+    # False if the first letter is not in pattern argument
+    # True if the entire pattern is crossed
     if i == 0 and pattern[0] in S:
         pattern_in_S = True
     return pattern_in_S
 
 
 if __name__ == "__main__":
-    tirade = ("""Mais, vous savez, moi je ne crois pas qu’il y ait de
-    bonne ou de mauvaise situation. Moi, si je devais résumer ma vie
-    aujourd’hui avec vous, je dirais que c’est d’abord des rencontres,
-    des gens qui m’ont tendu la main, peut-être à un moment où je ne
-    pouvais pas, où j’étais seul chez moi. Et c’est assez curieux de se
-    dire que les hasards, les rencontres forgent une destinée… Parce
-    que quand on a le goût de la chose, quand on a le goût de la chose
-    bien faite, le beau geste, parfois on ne trouve pas l’interlocuteur
-    en face, je dirais, le miroir qui vous aide à avancer. Alors ce
-    n’est pas mon cas, comme je le disais là, puisque moi au contraire,
-    j’ai pu ; et je dis merci à la vie, je lui dis merci, je chante la
-    vie, je danse la vie… Je ne suis qu’amour ! Et finalement, quand
-    beaucoup de gens aujourd’hui me disent « Mais comment fais-tu pour
-    avoir cette humanité ? », eh ben je leur réponds très simplement,
-    je leur dis que c’est ce goût de l’amour, ce goût donc qui m’a
-    poussé aujourd’hui à entreprendre une construction mécanique, mais
-    demain, qui sait, peut-être seulement à me mettre au service de la
-    communauté, à faire le don, le don de soi""")
+    T = "ACATACAGATG"
 
-    print(tirade)
+    bwtT = bwt(T)
+    print(bwtT)
 
-    bwtTirade = bwt(tirade)
-    print(bwtTirade)
+    print(suffix_table(T))
