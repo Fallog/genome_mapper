e66783530758151e913d393fa7f8b68dc51bd11e
f2ire <yanisfiire@gmail.com>
2022-12-03 22:25:46 +0100

    [UPDATE] Update some var name and docstring

--- y_dc3.py
+++ y_dc3.py
@@ -37,15 +37,29 @@ def makeTriplet(t_list: array, p12_list: array) -> array:
 
 
 def makeR0(p0, tp, index):
+    """Make R0 : list of list of 2 int. In the following form :
+    r0[i][0] = T[p0[i]] with T = T or T' (tp) if not the first iteration
+    r0[i][0] = index of p0[i]+1 in the list of index of R12 after sort
+
+    Args:
+        p0 (array): all index of T / T' i such that i%3=0
+        tp (array): T or T' if not the first iteration
+        index (array): list of index of R12 after sort
+
+    Returns:
+        array: The r0 list (follow rules above)
+    """
     y = p0.size
     r0 = np.empty((y), dtype=[("1", int), ("2", int)])
-    for i in range(y):  # TODO: TO UPGRADE
+    for i in range(y):
         r0[i][0] = int(tp[p0[i]])
-        r0[i][1] = np.where(index == p0[i] + 1)[0][0] + 1  # NOT SURE
+        r0[i][1] = (
+            np.where(index == p0[i] + 1)[0][0] + 1
+        )  # TODO: Maybe find a better way
     return r0
 
 
-def computeDc3Variable(T):
+def computeDc3Variable(T, sorting_algorithm):
     """
     Compute all DC3 variable computed in the first step and return it
 
@@ -56,17 +70,18 @@ def computeDc3Variable(T):
         p12 (array): p12 array (Index of T kept with n+1 and n+2 % 3)
         r12 (array): Triplet array no sorted
         r12s (array): Triplet array sorted
-        index (array): TODO: FIND WAY TO EXPLAIN THIS
-        tp (array): Array of index for each triplet after the sort, when it was before (T\')
+        index (array): list of index of R12 after sort
     """
 
     p12 = makeP12(T)
     r12 = makeTriplet(T, p12)
     r12s = np.copy(r12)
-    index_list = np.argsort(r12s, kind="mergesort", order=("1", "2", "3"))
+    index_list = np.argsort(r12s, kind=sorting_algorithm, order=("1", "2", "3"))
     index = np.take_along_axis(p12, index_list, axis=0)
 
-    r12s.sort(kind="quicksort", order=("1", "2", "3"))  # SORT HERE
+    r12s.sort(
+        kind="quicksort", order=("1", "2", "3")
+    )  # TODO: change the sort to optimize ?
 
     return p12, r12, r12s, index
 
@@ -79,23 +94,26 @@ def make_tp(r12s, r12):
     return tp
 
 
-def reccursive_sort_s11(init_seq, iter=0, print_var=False):
+def reccursive_sort_s11(init_seq, sorting_algorithm, iter=0, print_var=False):
     """Do the reccusrive sort to eliminate all value in double
 
     Args:
         init_seq (array): T string
-        last_tp (_type_): Last T\'.
         iter (int, optional): The actual number of iteration. Defaults to 0.
+        print_var(Bool, optional): If true, print var of DC3 (not usefull)
+
+    return:
+        array: Suffix array sorted
     """
     T = np.concatenate((init_seq, np.zeros(3)))
-    p12, r12, r12s, index = computeDc3Variable(T)
-    if r12.shape != np.unique(r12, axis=0).shape:  # step 2 ?
+    p12, r12, r12s, index = computeDc3Variable(T, sorting_algorithm)
+    if r12.shape != np.unique(r12, axis=0).shape:
         tp = make_tp(r12s, r12)
         i = iter + 1
-        index012 = reccursive_sort_s11(tp, i)
-        index = np.take_along_axis(p12, index012, axis=0)  # r12 = index12 sur mon cours
+        index012 = reccursive_sort_s11(tp, sorting_algorithm, i)
+        index = np.take_along_axis(p12, index012, axis=0)
     if __name__ == "__main__" and print_var:
-        tools.printDc3Var(p12, r12, r12s, index, T, iter)  # Pour vérifier
+        tools.printDc3Var(p12, r12, r12s, index, T, iter)  # To check
     if iter:
         p0 = np.arange(0, init_seq.size, 3)
         r0 = makeR0(p0, init_seq, index)
@@ -110,6 +128,18 @@ def reccursive_sort_s11(init_seq, iter=0, print_var=False):
 
 
 def step2(T, index_r12, r0, p0):
+    """Step of merging to create a new index array updated for the next iteration
+
+        Args:
+            T (array): Array of int corresponding to the str sequence for the i iteration
+    with the sentinels 0
+            index_r12 (array): list of index of R12 after sort
+            r0 (array): r0 array (check make_r0)
+            p0 (array): all index of T / T' i such that i%3=0
+
+        Returns:
+            array: New index array updated for the next iteration
+    """
     index_r0 = np.argsort(
         r0, kind="mergesort", order=("1", "2")
     )  # Give index after sort
@@ -120,7 +150,20 @@ def step2(T, index_r12, r0, p0):
     return index012
 
 
-def get_smallest_index(T, val_12, val_0, r12):  # TODO: ULTRA MOCHE MAIS CA MARCHE
+def get_smallest_index(T, val_12, val_0, index_r12):  # TODO: Maybe change to optimize
+    """Algorithm to get the smallest value adapted to DC3 necessity
+    Its needed to merge r0 and r12 when value are equals
+
+    Args:
+        T (array): Array of int corresponding to the str sequence for the i iteration
+    with the sentinels 0
+        val_12 (int): current value in p12
+        val_0 (int): current value in p0
+        index_r12 (array): list of index of R12 after sort
+
+    Returns:
+        tuple: (value,"0" if belong to p0, else "12" (such that correspond to p12))
+    """
     T_12 = T[val_12]
     T_0 = T[val_0]
     if T_12 != T_0:
@@ -131,8 +174,11 @@ def get_smallest_index(T, val_12, val_0, r12):  # TODO: ULTRA MOCHE MAIS CA MARC
     else:
         if val_12 % 3 == 1 or val_0 % 3 == 1:
             if (
-                min(np.where(r12 == val_12 + 1)[0][0], np.where(r12 == val_0 + 1)[0][0])
-                == np.where(r12 == val_0 + 1)[0][0]
+                min(
+                    np.where(index_r12 == val_12 + 1)[0][0],
+                    np.where(index_r12 == val_0 + 1)[0][0],
+                )
+                == np.where(index_r12 == val_0 + 1)[0][0]
             ):  # We check smallest value bc already ordered in r12
                 return (val_0, "0")
             else:
@@ -148,10 +194,10 @@ def get_smallest_index(T, val_12, val_0, r12):  # TODO: ULTRA MOCHE MAIS CA MARC
             else:
                 if (
                     min(
-                        np.where(r12 == val_12 + 2)[0][0],
-                        np.where(r12 == val_0 + 2)[0][0],
+                        np.where(index_r12 == val_12 + 2)[0][0],
+                        np.where(index_r12 == val_0 + 2)[0][0],
                     )
-                    == np.where(r12 == val_0 + 2)[0][0]
+                    == np.where(index_r12 == val_0 + 2)[0][0]
                 ):  # We check smallest value bc already ordered in r12
                     return (val_0, "0")
                 else:
@@ -159,6 +205,17 @@ def get_smallest_index(T, val_12, val_0, r12):  # TODO: ULTRA MOCHE MAIS CA MARC
 
 
 def merging_r0_r12(T, index_r0, index_r12):
+    """Merge r0 and r12 index to update the good index for the next iteration
+
+    Args:
+        T (array): Array of int corresponding to the str sequence for the i iteration
+    with the sentinels 0
+        index_r0 (_type_): is r0 (check make_r0)
+        index_r12 (array): list of index of R12 after sort
+
+    Returns:
+        array: index array updated for the next iteration
+    """
     size12, size0 = index_r12.size, index_r0.size
     index_table_merged = np.empty(size12 + size0, dtype=int)
     i_r12, i_r0 = 0, 0
@@ -185,9 +242,9 @@ def merging_r0_r12(T, index_r0, index_r12):
         return index_table_merged[1:]  # Eleminate centinel
 
 
-def dc3(seq: str):  # TODO: Changer recursive_sort_s11 en DC3
+def dc3(seq: str, sorting_algorithm: str = "mergesort"):
     T = tools.strToAscii(seq)
-    suffix_array = reccursive_sort_s11(T)
+    suffix_array = reccursive_sort_s11(T, sorting_algorithm)
     return suffix_array
 
 
