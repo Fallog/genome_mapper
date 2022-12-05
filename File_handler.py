2dd4af36831dbcbba735444830a243dcef697082
f2ire <yanisfiire@gmail.com>
2022-12-05 13:39:47 +0100

    [UPDATE] add import method
    -- Now import_dc3_result that can import suffixe table from .json

--- File_handler.py
+++ File_handler.py
@@ -11,24 +11,47 @@ class File_handler:
                 dc3 result form : SPECIES_CHR_NUMBER
         """
         self.file_name = file_name
-        self.dc3_path = "RESULTS/dc3_result"
+        self.dc3_path = f"RESULTS/dc3_result_{file_name}.json"
 
-    def export_dc3_result(self, suffix_table):
-        path_file = f"{self.dc3_path}_{self.file_name}"
-        if os.path.isfile(path_file):
-            answer = 0
-            while answer not in ("y", "n"):
-                answer = input(
+    def export_dc3_result(self, suffix_table, overwrite="y") -> None:
+        """Create a .json file contening our dc3 results
+
+        Args:
+            suffix_table (array): result of dc3 algorithm
+            overwrite (str, optional):
+                If "y" and file already exist, it is overwrited.
+                If "n", it is not.
+                If something else, it ask what the user want
+                Defaults to "y".
+        """
+        if os.path.isfile(self.dc3_path):
+            overwrite = 0
+            while overwrite not in ("y", "n"):
+                overwrite = input(
                     "Do you want to overwrite old dc3 result for this information ? (y/n) : "
-                )
-            if answer.lower() == "y":
-                with open(path_file, "w") as file:
+                ).lower()
+            if overwrite.lower() == "y":
+                with open(self.dc3_path, "w") as file:
                     json.dump(suffix_table, file)
         else:
-            with open(path_file, "w") as file:
+            with open(self.dc3_path, "w") as file:
                 json.dump(suffix_table, file)
+
+    def import_dc3_result(self):
+        """Return the suffixe table
+
+        Returns:
+            array: Suffixe table.
+        """
+        if os.path.isfile(self.dc3_path):
+            with open(self.dc3_path, "r") as file:
+                return json.load(file)
+        else:
+            print(f"No file a the path {self.dc3_path}.")
 
 
 if __name__ == "__main__":
     a = File_handler("test_1")
     a.export_dc3_result([4, 3, 2, 6])
+    table = a.import_dc3_result()
+    print(table)
