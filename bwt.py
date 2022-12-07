8629f1b29c631464e2c4d31bcf227764e40968d4
Fallog <fallog@mailo.com>
2022-12-07 09:51:48 +0100

    [UPDATE] search_kmer_pos - Function is now returning an array with all the position of the kmer in the DNA sequence, along with the kmer itself and the number of occurences.

--- bwt.py
+++ bwt.py
@@ -1,5 +1,6 @@
 import numpy as np
-from copy import deepcopy
+from File_handler import File_handler
+from Bio import SeqIO
 
 
 def bwt(dna, suffixTable, end_of_string="$"):
@@ -18,8 +19,9 @@ def bwt(dna, suffixTable, end_of_string="$"):
     bwtStr = ""
     dna += end_of_string
 
-    for suffix_pos in suffixTable:
-        bwtStr += dna[suffix_pos - 1]
+    for i in range(len(suffixTable)):
+        # Chromosomes have sometimes bases in lowercase
+        bwtStr += dna[suffixTable[i] - 1].capitalize()
 
     return bwtStr
 
@@ -57,31 +59,36 @@ def create_rank_mat(bwtDna):
     return rkMat
 
 
-def search_kmer_pos(bwtDna, kmer, suffixTab):
-    """Search a kmer in a Burrows Wheeler tranformed genome. Returns
-    the kmer and it's position(s) in the genome.
+def search_kmer_pos(bwtDna, rankMat, suffixTab, kmer):
+    """Search a kmer in a Burrows Wheeler tranformed chromosome.
+    Returns the kmer and it's position(s) in the genome. If the kmer is
+    absent, it returns the kmer alone.
 
     Args:
         bwtDna (str): the BWT of a string made only with A, T, C, G
             and a $
-        kmer (str): nucleotides pattern to search in the genome
+        rankMat (dict): contains 5 keys: A, T, C, G and $, each key is
+            mapped with the rank table in the dnaSeq of the associated
+            character
         suffixTab (ndarray): sorted array of all the suffixes
             used to built bwtDna
+        kmer (str): nucleotides pattern to search in the genome
 
     Return:
-        bool: true if the pattern is in the string
+        str: kmer argument
+        int: number of times kmer can be read on the DNA sequence
+        ndarray: every localisation where kmer can be read
     """
-    isKmerIn = False
-
-    bwtSort = deepcopy(bwtDna).sort()  # Avoid modifying bwtGen while sorting
+    # Avoid modifying bwtGen while sorting
+    bwtSort = sorted(list(bwtDna))
     lenBwt = len(bwtDna)
+    # print(f"Sorted BWT: {bwtSort}")
 
     e = 0
     f = lenBwt - 1  # Stay in the string boundaries
     i = len(kmer) - 1  # Stay in the kmer boundaries
+    print(f"first e: {e},  f: {f}")
 
-    rankMat = create_rank_mat(bwtDna)
-    # print(f"Rank matrix: {rankMat}")
     nbOccur = 1
 
     while nbOccur > 0 and i >= 0:
@@ -90,60 +97,89 @@ def search_kmer_pos(bwtDna, kmer, suffixTab):
         if X not in bwtDna:
             return False
         else:
-            # print(f"i: {i}, X: {X}")
-            # print(f"Rank table of {X}: {rankMat[X]}")
+            print(f"i: {i}, X: {X}")
+            print(f"Rank table of {X}: {rankMat[X]}")
 
             firstOcc = bwtSort.index(X) - 1  # $ isn't taken into account
-            # print(f"First index of {X} in bwtSort: {firstOcc}")
+            print(f"First index of {X} in bwtSort: {firstOcc}")
 
+            # Counts all the occurences of X in bwtDna between an empty
+            # rank and the following rank to deduce the rank of an
+            # actual index e
             newE = e
-            rank = rankMat[X][newE]
-            counts = 0
-            while rank == 0:
-                newE -= 1
+            rankE = rankMat[X][newE]
+            countE = 0
+            while rankE == 0:
+                newE += 1
                 if bwtDna[newE] == X:
-                    counts -= 1
-                rank = rankMat[X][newE]
-            rank -= counts
+                    countE += 1
+                rankE = rankMat[X][newE]
+            rankE -= countE
+            print(f"rankE: {rankE}")
 
-            # TODO Do the same but for f
+            newF = f
+            rankF = rankMat[X][newF]
+            countF = 0
+            while rankF == 0:
+                newF -= 1
+                if bwtDna[newF] == X:
+                    countF += 1
+                rankF = rankMat[X][newF]
+            rankF -= countF
+            print(f"rankF: {rankF}")
 
             # The first character of the BWT has a rank of 1 in the
             # rank matrix but it is the first appearance of this
             # character. To take into account this information, we
             # decrease e of 1 in presence of this character, i.e when
             # there is no character before it
-            # TODO integrate this part in the while above
             if len(bwtDna[:(e + firstOcc)]) == 0:
-                e = firstOcc + rankMat[X][e] - 1
+                e = firstOcc + rankE - 1
             else:
-                e = firstOcc + rankMat[X][e]
-            f = firstOcc + rankMat[X][f]
-            # print(f"e: {e}, f: {f}")
+                e = firstOcc + rankE
+            f = firstOcc + rankF
+            print(f"e: {e}, f: {f}")
 
             nbOccur = f - e  # quantity of elements between 2 indexes
+
             if nbOccur == 0:
                 return False
-            # print(f"Number of pattern: {nbOccur}\n")
+            print(f"Number of pattern: {nbOccur}\n")
+
+            locs = suffixTab[e:f]  # positions of the kmer in the chromosome
 
             i -= 1
 
             # True if the entire pattern is crossed
             if i == -1:
-                isKmerIn = True
-                return isKmerIn, nbOccur
+                return kmer, nbOccur, locs
 
-    return isKmerIn, nbOccur
+    return kmer
 
 
 if __name__ == "__main__":
-    T = "ATAATAGGATCCGA" * 500
-    rankMat = create_rank_mat(T)
-    disp = "[ "
-    for count in rankMat['A']:
-        disp += f" {count}"
-    disp += "]"
-    print(disp)
+    chromo = []
+    for record in SeqIO.parse("SEQUENCES/P_fal_genome.fna", format="fasta"):
+        chromo.append(str(record.seq))
+    # print(f"chromosome 1: {chromo[0]}")
+
+    s_table = File_handler("P_fal_chromosome_1").import_dc3_result()
+    # print(f"Suffix table: {s_table}")
+
+    bwtChromo1 = bwt(chromo[0], s_table)  # testing for the first chromosome
+    # print(f"bwt: {bwtChromo1[:100]}, type: {type(bwtChromo1)}")
+
+    bwtList = list(bwtChromo1)
+    # print(f"Sorted BWT list: {sorted(bwtList)[:100]}")
+
+    rankMat = create_rank_mat(bwtChromo1)
+    print(f"Rank matrix of A: {rankMat['A'][260000:261000]}")
+
+    # disp = "[ "
+    # for count in rankMat['A']:
+    #     disp += f" {count}"
+    # disp += "]"
+    # print(disp)
 
     # bwtT = bwt(T)
     # print(f"BWT from suffix table: {bwtT}")
@@ -152,4 +188,4 @@ if __name__ == "__main__":
     # print(f"Is the BWT DC3 the same ? {bwtDC3 == bwtT}")
     # print(f"BWT DC3: {bwtDC3}")
 
-    # print(search_kmer_pos(T, "ATA"))
+    print(search_kmer_pos(bwtChromo1, rankMat, s_table, "ATA"))
