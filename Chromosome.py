3a2625c187dba42f469da4d937fdbe5bb42c4aed
f2ire <yanisfiire@gmail.com>
2022-12-13 12:29:10 +0100

    [UPDATE] All new result for inverse comp seq
    - rank_mat fun added to classe Chromosome
    - Result for reverse-comp sequence

--- Chromosome.py
+++ Chromosome.py
@@ -18,10 +18,16 @@ class Chromosome:
         self.DNA_dol = seq + "$"
         self.dc3_path = f"RESULTS/dc3_result_{file_name}.npy"
         self.bwt_path = f"RESULTS/bwt_result_{file_name}.txt"
+        self.rank_mat_path = f"RESULTS/rank_mat_result_{file_name}.npy"
         self.suffix_table = None
         self.bwt = None
+        self.rank_mat = None
         if os.path.isfile(self.dc3_path):
             self.import_dc3_result()
+        if os.path.isfile(self.bwt_path):
+            self.import_bwt_result()
+        if os.path.isfile(self.rank_mat_path):
+            self.import_rank_mat_result()
 
     def export_dc3_result(self, suffix_table, overwrite="y") -> None:
         """Create a .npy file contening our dc3 results
@@ -110,13 +116,57 @@ class Chromosome:
         else:
             print(f"No file at the path {self.bwt_path}.")
 
+    def export_rank_matrix_result(self, rank_mat=None, overwrite="y") -> None:
+        """Create a .npy file contening our bwt results
+
+        Args:
+            suffix_table (array): result of bwt algorithm
+            overwrite (str, optional):
+                If "y" and file already exist, it is overwrited.
+                If "n", it is not.
+                If something else, it ask what the user want
+                Defaults to "y".
+        """
+        if os.path.isfile(self.dc3_path):
+            while overwrite not in ("y", "n"):
+                overwrite = input(
+                    "Do you want to overwrite old dc3 result for this information ? (y/n) : "
+                ).lower()
+            if overwrite.lower() == "y":
+                if rank_mat is None:
+                    np.save(self.rank_mat_path, rank_mat)
+
+                else:
+                    self.rank_mat = rank_mat
+                    np.save(self.rank_mat_path, rank_mat)
+        else:
+            self.rank_mat = rank_mat
+            np.save(self.rank_mat_path, rank_mat)
+
+    def import_rank_mat_result(self, ret=True):
+        """Return the suffixe table
+
+        Returns:
+            array: Suffixe table.
+        """
+        if os.path.isfile(self.rank_mat_path):
+            if ret:
+                self.rank_mat = np.load(self.rank_mat_path, allow_pickle=True)
+                return self.rank_mat
+            else:
+                self.rank_mat = np.load(self.rank_mat_path, allow_pickle=True)
+        else:
+            print(f"No file at the path {self.rank_mat_path}.")
+
 
 if __name__ == "__main__":
     a = Chromosome("test_1", "ezad", "4")
     a.export_dc3_result([4, 3, 2, 6])
     table = a.import_dc3_result()
-    a.import_bwt_result()
+    # a.export_rank_matrix_result({"a": 21, "b": 55})
+    a.import_rank_mat_result()
     print(a.bwt, "bwt")
     print(a.suffix_table)
+    print(a.rank_mat)
 
     print(table)
--- complexity_test.py
+++ complexity_test.py
@@ -6,37 +6,51 @@ import dc3_test
 import numpy as np
 from Sais import Sais
 import tools
-
-# seq = "AGAGAGGAGCGCGCGAGAGCGCGAGAG" * 500
-# seq += "$"
-# random.seed(12)
-seq = ""
-for i in range(4000000):
-    seq += random.choice(["A", "C", "G", "T"])
-seq += "$"
-# seq = "ACGTGCCTAGCCTACCGTGAAACCGGCGGCGGGAAGCGCGAAAGAGAGAGCGGAGAGAGGCGCGCC$"
-# print(seq)
-# print(y.dc3(seq))
-
-cProfile.run("""y.dc3(seq, sorting_algorithm = "stable")""")
-# print(y.dc3(seq, Tru2
-a = y.dc3(seq, "mergesort")
-
-## SAIS TEST : PAS OPTI FINALLEMENT
-# test = Sais()
-# d = tools.strToBase(seq) - 1
-# a = test.build_suffix_array(d)
-# cProfile.run("""test.build_suffix_array(d)""")
-
-cProfile.run("""dc3_test.skew(seq)""")
-b = dc3_test.skew(seq)
-
-c = np.where((a == b) == False)
-
-print(c)
-# print(c[0].size)
-# print(a == b)
+import bwt
 
 
-## 44 sec : 600 000
-## 5 minutes : 3M3
+def test_dc3():
+    # seq = "AGAGAGGAGCGCGCGAGAGCGCGAGAG" * 500
+    # seq += "$"
+    # random.seed(12)
+    seq = ""
+    for i in range(600000):
+        seq += random.choice(["A", "C", "G", "T"])
+    seq += "$"
+    # seq = "ACGTGCCTAGCCTACCGTGAAACCGGCGGCGGGAAGCGCGAAAGAGAGAGCGGAGAGAGGCGCGCC$"
+    # print(seq)
+    # print(y.dc3(seq))
+
+    cProfile.run("""y.dc3(seq, sorting_algorithm = "stable")""")
+    # print(y.dc3(seq, Tru2
+    a = y.dc3(seq, "mergesort")
+
+    ## SAIS TEST : PAS OPTI FINALLEMENT
+    # test = Sais()
+    # d = tools.strToBase(seq) - 1
+    # a = test.build_suffix_array(d)
+    # cProfile.run("""test.build_suffix_array(d)""")
+
+    cProfile.run("""dc3_test.skew(seq)""")
+    b = dc3_test.skew(seq)
+
+    c = np.where((a == b) == False)
+    # print(c[0].size)
+    # print(a == b)
+    print(c)
+
+
+def test_rank_mat():
+    seq = ""
+    for i in range(1000000):
+        seq += random.choice(["A", "C", "G", "T"])
+    seq += "$"
+    st = y.dc3(seq)
+    bwtseq = bwt.bwt(seq, st)
+    # print(bwtseq)
+    print("BWT DONE !")
+    return bwtseq
+
+
+a = test_rank_mat()
+cProfile.run("""bwt.create_rank_mat(a)""")
